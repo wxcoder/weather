c54e37d975fbe32d36685bc97720c8ecc5dec554
wxcoder <car.mcnaught@gmail.com>
2015-06-02 23:53:22 -0400

    modified:   functional_tests.py

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,26 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+    	self.browser.quit()
+
+    def test_can_click_slideshow(self):
+   		# Check out the home page
+   		self.browser.get('http://localhost:8000')
+
+   		# Check page title and header mention website name
+   		self.assertIn('WeatherPoint', self.browser.title)
+   		self.fail('Finish the test!')
+
+   		# Opens the page to a slideshow
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
+
+
