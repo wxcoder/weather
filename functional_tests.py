4e5d68cd825f24b19dd3e918b46441b33c981279
wxcoder <car.mcnaught@gmail.com>
2015-06-03 04:28:22 -0400

    Functional test checks slideshow initial image

--- functional_tests.py
+++ functional_tests.py
@@ -10,15 +10,25 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
     	self.browser.quit()
 
-    def test_can_click_slideshow(self):
+    def test_slideshow_initial_image_goes_to_model_page(self):
    		# Check out the home page
    		self.browser.get('http://localhost:8000')
 
    		# Check page title and header mention website name
    		self.assertIn('WeatherPoint', self.browser.title)
+
+   		self.browser.find_element_by_id('firstimg').click()
+
+   		self.assertIn('Models Page', self.browser.title)
+
+   		self.browser.find_element_by_id('home').click()
+   		
+   		self.assertIn('WeatherPoint', self.browser)
+
    		self.fail('Finish the test!')
 
    		# Opens the page to a slideshow
+
 
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
